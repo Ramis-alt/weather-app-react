ee7ddc82dfdd3d45aec0927697556e0e8fb9edc4
Ramiro Renteria <Ramis28@yahoo.com>
2023-01-15 17:33:08 -0800

    final commit to the weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
+//import React, {useState} from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import React from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=5fa5eb8699748761236860568f29ab0f`
 
+const searchLocation = (event) => {
+  //since there is no button, we're using an event listener for an enter key
+  if(event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+  
+}
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className='search'>
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        placeholder="Enter Location"
+        onKeyDown={searchLocation}
+        type="text"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            {data.main ? <h1>{data.main.temp.toFixed()} °F</h1> : null}
+          
+          </div>
+          <div className='description'>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            {data.main ? <p className='bold'>{data.main.feels_like.toFixed()} °F</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className='humidity'>
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className='wind'>
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
 export default App
+
+//5fa5eb8699748761236860568f29ab0f
+//e2f367ebce58cb7c829b25890bc284d7
+//https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco,us&APPID=5fa5eb8699748761236860568f29ab0f
+//https://api.openweathermap.org/data/2.5/weather?q=Dallas&APPID=5fa5eb8699748761236860568f29ab0f
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
